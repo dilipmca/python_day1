20da71fa553b94e1bc98f0f1e4110a82958d38cc
dilipmca <53147509+dilipmca@users.noreply.github.com>
2019-07-22 00:08:23 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("hello world")
+
+
+# In[15]:
+
+
+x=10
+y=5
+x+y 
+
+
+# In[13]:
+
+
+message = "hello python world"
+print(message)
+
+
+# In[16]:
+
+
+name = "dilip kumar"
+print (name)
+
+
+# In[19]:
+
+
+first_name ="dilip"
+last_name = "kumar"
+print(first_name)
+
+
+# In[21]:
+
+
+first_name ="dilip"
+last_name = "kumar"
+print(first_name)
+
+
+# In[23]:
+
+
+name="dilip kumar"
+cname="DILIP KUMAR"
+print(name.title())
+print(cname.title())
+
+
+# In[25]:
+
+
+name="dilip kumar"
+cname="DILIP KUMAR"
+print(name.upper())
+print(cname.lower())
+
+
+# In[27]:
+
+
+first_name ="Dilip"
+last_name = "kumar"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(last_name.title())
+print(full_name.title())
+
+
+# In[29]:
+
+
+#adding whitespace to strings with tabs and newlines..
+#whitespace:- any non printing character..!!
+
+print("python ")
+
+
+# In[31]:
+
+
+print('\tpython')
+
+
+# In[35]:
+
+
+declare = "testing 123"
+declare1 = 'testing 1234'
+declare2 = """testing12345"""
+example =  'dilip\'s laptop'
+example1 =  'dilip\'s laptop \ computer'
+print(declare)
+print(declare1)
+print(declare2)
+print(example)
+print(example1)
+
+
+# In[37]:
+
+
+print("Dilip Kumar\nWorking\nBangalore")
+
+
+# In[44]:
+
+
+fav_lang = ' dilip '
+print(fav_lang.lstrip())
+print(fav_lang.rstrip().upper())
+print(fav_lang.rstrip().lstrip().title())
+
+
+# In[ ]:
+
+
+
+
